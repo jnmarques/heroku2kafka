88c497f9ad15723d2c0bd26ad2562189ca66d538
jnmarques <zecanelsonpm@gmail.com>
2023-08-02 17:16:48 +0100

    Added security Configs

--- src/main/java/pt/altice/heroku2kafka/extract/SourceKafkaConsumer.java
+++ src/main/java/pt/altice/heroku2kafka/extract/SourceKafkaConsumer.java
@@ -7,12 +7,15 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -47,6 +50,41 @@ public class SourceKafkaConsumer {
     @Value("${source." + ConsumerConfig.AUTO_OFFSET_RESET_CONFIG + ":earliest}")
     public String autoOffsetReset;
 
+    // * Security Configurations */
+
+    @Value("${source." + CommonClientConfigs.SECURITY_PROTOCOL_CONFIG + ":#{null}}")
+    public String securityProtocol;
+
+    @Value("${source." + SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG + ":#{null}}")
+    public String sslTruststoreLocation;
+
+    @Value("${source." + SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG + ":#{null}}")
+    public String sslTruststorePassword;
+
+    @Value("${source." + SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG + ":#{null}}")
+    public String sslTruststoreType;
+
+    @Value("${source." + SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG + ":#{null}}")
+    public String sslKeystoreLocation;
+
+    @Value("${source." + SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG + ":#{null}}")
+    public String sslKeystorePassword;
+
+    @Value("${source." + SslConfigs.SSL_KEYSTORE_TYPE_CONFIG + ":#{null}}")
+    public String sslKeystoreType;
+
+    @Value("${source." + SslConfigs.SSL_KEY_PASSWORD_CONFIG + ":#{null}}")
+    public String sslKeyPassword;
+
+    @Value("${source." + SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG + ":#{null}}")
+    public String sslEndpointIdentificationAlgorithm;
+
+    @Value("${source." + SaslConfigs.SASL_MECHANISM + ":#{null}}")
+    public String saslMechanism;
+
+    @Value("${source." + SaslConfigs.SASL_JAAS_CONFIG + ":#{null}}")
+    public String saslJaasConfig;
+
     private String keyDeserializer = "org.apache.kafka.common.serialization.StringDeserializer";
 
     private String valueDeserializer = "org.apache.kafka.common.serialization.StringDeserializer";
@@ -74,6 +112,40 @@ public class SourceKafkaConsumer {
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
+        if(securityProtocol != null) {
+            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        }
+        if(sslTruststoreLocation != null) {
+            props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, sslTruststoreLocation);
+        }
+        if(sslTruststorePassword != null) {
+            props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, sslTruststorePassword);
+        }
+        if(sslTruststoreType != null) {
+            props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, sslTruststoreType);
+        }
+        if(sslKeystoreLocation != null) {
+            props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, sslKeystoreLocation);
+        }
+        if(sslKeystorePassword != null) {
+            props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, sslKeystorePassword);
+        }
+        if(sslKeystoreType != null) {
+            props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, sslKeystoreType);
+        }
+        if(sslKeyPassword != null) {
+            props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, sslKeyPassword);
+        }
+        if(sslEndpointIdentificationAlgorithm != null) {
+            props.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, sslEndpointIdentificationAlgorithm);
+        }
+        if(saslMechanism != null) {
+            props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+        }
+        if(saslJaasConfig != null) {
+            props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        }
+        
 
         consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList(topic));
--- src/main/java/pt/altice/heroku2kafka/load/SinkKafkaProducer.java
+++ src/main/java/pt/altice/heroku2kafka/load/SinkKafkaProducer.java
@@ -3,9 +3,12 @@ package pt.altice.heroku2kafka.load;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,8 +32,43 @@ public class SinkKafkaProducer {
     public String idempotence;
 
     private String keySerializer = "org.apache.kafka.common.serialization.StringSerializer";
-    
+
     private String valueSerializer = "org.apache.kafka.common.serialization.StringSerializer";
+
+    // * Security Configurations */
+
+    @Value("${sink." + CommonClientConfigs.SECURITY_PROTOCOL_CONFIG + ":#{null}}")
+    public String securityProtocol;
+
+    @Value("${sink." + SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG + ":#{null}}")
+    public String sslTruststoreLocation;
+
+    @Value("${sink." + SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG + ":#{null}}")
+    public String sslTruststorePassword;
+
+    @Value("${sink." + SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG + ":#{null}}")
+    public String sslTruststoreType;
+
+    @Value("${sink." + SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG + ":#{null}}")
+    public String sslKeystoreLocation;
+
+    @Value("${sink." + SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG + ":#{null}}")
+    public String sslKeystorePassword;
+
+    @Value("${sink." + SslConfigs.SSL_KEYSTORE_TYPE_CONFIG + ":#{null}}")
+    public String sslKeystoreType;
+
+    @Value("${sink." + SslConfigs.SSL_KEY_PASSWORD_CONFIG + ":#{null}}")
+    public String sslKeyPassword;
+
+    @Value("${sink." + SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG + ":#{null}}")
+    public String sslEndpointIdentificationAlgorithm;
+
+    @Value("${sink." + SaslConfigs.SASL_MECHANISM + ":#{null}}")
+    public String saslMechanism;
+
+    @Value("${sink." + SaslConfigs.SASL_JAAS_CONFIG + ":#{null}}")
+    public String saslJaasConfig;
 
     KafkaProducer<String, String> producer;
     Logger logger = LoggerFactory.getLogger(SinkKafkaProducer.class);
@@ -46,6 +84,39 @@ public class SinkKafkaProducer {
         props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        if (securityProtocol != null) {
+            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        }
+        if (sslTruststoreLocation != null) {
+            props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, sslTruststoreLocation);
+        }
+        if (sslTruststorePassword != null) {
+            props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, sslTruststorePassword);
+        }
+        if (sslTruststoreType != null) {
+            props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, sslTruststoreType);
+        }
+        if (sslKeystoreLocation != null) {
+            props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, sslKeystoreLocation);
+        }
+        if (sslKeystorePassword != null) {
+            props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, sslKeystorePassword);
+        }
+        if (sslKeystoreType != null) {
+            props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, sslKeystoreType);
+        }
+        if (sslKeyPassword != null) {
+            props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, sslKeyPassword);
+        }
+        if (sslEndpointIdentificationAlgorithm != null) {
+            props.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, sslEndpointIdentificationAlgorithm);
+        }
+        if (saslMechanism != null) {
+            props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+        }
+        if (saslJaasConfig != null) {
+            props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        }
 
         producer = new KafkaProducer<>(props);
     }
